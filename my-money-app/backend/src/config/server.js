34592eab23173c4b393195e84db636a20b9fca35
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-29 11:46:15 -0300

    chore: Initial project setup.

---
+++ my-money-app/backend/src/config/server.js
@@ -0,0 +1,14 @@
+//imports
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express();
+
+//middlewares
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+//port config
+const port = 3006;
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`);
+});
